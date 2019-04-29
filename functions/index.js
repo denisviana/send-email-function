aab63e575bd8c8267d82ac53c4021c33afcdbcff
Denis Costa <denis.costa@snowmanlabs.com>
2019-04-29 18:00:57 -0300

    Some adjustments

--- functions/index.js
+++ functions/index.js
@@ -1,75 +1,61 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
-const db = admin.firestore();
-const axios = require('axios')
-const { api_key } = 'key-us20'
+var fetch = require('isomorphic-fetch')
+var btoa = require('btoa');
 
-const cors = require('cors')({
-    origin: true,
-});
+admin.initializeApp();
 
 exports.onNewUserRegistered = functions.firestore
 .document('users/{userId}')
 .onCreate((snap, context) => {
 
+  const hiredList = 'idList1';
+  const hirerList = 'idList2';
+
     var user = snap.data();
+
     console.log(user);
     console.log('checking user type...');
-    if(user.comprador){
-        console.log('subscribing user...');
-        subscribeUser(user.email, user.firstName, user.lastName).then(response => {
-            console.log('successful');
-            return Promise.all();
-        }).catch(error =>{
-            console.log('error :'+error);
-            return Promise.all();
-        });
-
+    if(user.role === "contratado"){
+      console.log('subscribing user on list '+hiredList);
+        return subscribeUser(user.fullName,user.email,hiredList)
+    }else if(user.role === "contratante"){
+      console.log('subscribing user on list '+hirerList);
+        return subscribeUser(user.fullName,user.email,hirerList)
     }else
         console.log('exiting...');
 
 return Promise.all();
 });
 
-function subscribeUser (emailAddress, firstName, lastName) {
-
-  const mailchimpApiUrl = 'https://usxx.api.mailchimp.com/3.0';
-  const listID = 'id';
-
-  const options = {
-        method: 'POST',
-        url: mailchimpApiUrl+'/lists/'+listID+'/members/',
-        headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-    auth: {
-      username: 'anystring',
-      password: api_key
-    },
-    data: {
-      email_address: emailAddress,
-      status: 'subscribed',
-      merge_fields: {
-        'FNAME': firstName,
-        'LNAME': lastName
-      }
-    }
+function subscribeUser(firstName,emailAddress, listID) {  
+  var MAILCHIMP_API_KEY = 'API_KEY'
+  var url = 'https://usxx.api.mailchimp.com/3.0/lists/' + listID + '/members'
+  var method = 'POST'
+  var headers = {
+    'authorization': "Basic " + btoa('randomstring:' + MAILCHIMP_API_KEY),
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
   }
-  
-  return axios(options)
+  var body = JSON.stringify({email_address: emailAddress,
+    status: 'subscribed',
+    merge_fields: {
+      'FNAME': firstName,
+    }
+  });
+
+  return fetch(url, {
+    method,
+    headers,
+    body
+  }).then(resp => {
+    console.log('successful');
+    resp.json();
+    return Promise.all();
+  })
+  .then(resp => {
+    console.log(resp)
+    return Promise.all();
+  })
 }
 
-/* 
-export const subscribeUser = async (request, res) => {
-    return cors(request, res, () => {
-      const { emailAddress, firstName, lastName } = request.body
-  
-      subscribeUserToMailchimp(emailAddress, firstName, lastName).then(response => {
-        // handle success logic
-      }).catch(error => {
-        // handle error logic
-      })
-    })
-  }
-  */
