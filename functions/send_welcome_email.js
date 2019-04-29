c776458a7e4eff5e120842b79e8f7966e5257e38
Denis Viana <denisvcosta@gmail.com>
2019-04-29 09:47:00 -0300

    Update send_welcome_email.js

--- functions/send_welcome_email.js
+++ functions/send_welcome_email.js
@@ -1,11 +1,11 @@
 
 const axios = require('axios')
-const { api_key } = 'b6f02a5596fd8689b535302be9b99a20-us20'//functions.config().mailchimp
+const { api_key } = 'key'//functions.config().mailchimp
 
 const subscribeUser = (emailAddress, firstName, lastName) => {
 
-    const mailchimpApiUrl = 'https://us20.api.mailchimp.com/3.0'
-    const listID = '9106632cb1'
+    const mailchimpApiUrl = 'https://usxx.api.mailchimp.com/3.0'
+    const listID = 'id'
     const options = {
           method: 'POST',
           url: `${mailchimpApiUrl}/lists/${listID}/members/`,
